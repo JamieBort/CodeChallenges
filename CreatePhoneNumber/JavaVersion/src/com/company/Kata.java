14bbb6047298cb5da637026ad14fd9aa2adee2b2
Jamie Bort <jamiebort@gmail.com>
2018-09-21 17:42:23 -0400

    Set up files for Java version of CreatePhoneNumber.

---
+++ CreatePhoneNumber/JavaVersion/src/com/company/Kata.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Kata {
+    public static String createPhoneNumber(int[] numbers) {
+        // Your code here!
+        System.out.println("Hello");
+        return null;
+    }
+}
