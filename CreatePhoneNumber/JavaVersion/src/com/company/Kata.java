ea1cb180199c13bbe3e10ace7774353411d97da0
Jamie Bort <jamiebort@gmail.com>
2018-09-21 18:21:11 -0400

    Completed Java version.

--- CreatePhoneNumber/JavaVersion/src/com/company/Kata.java
+++ CreatePhoneNumber/JavaVersion/src/com/company/Kata.java
@@ -3,7 +3,9 @@ package com.company;
 public class Kata {
     public static String createPhoneNumber(int[] numbers) {
         // Your code here!
-        System.out.println("Hello");
-        return null;
+        String str = "(" + numbers[0] + numbers[1] +  numbers[2] + ") " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+        System.out.println(str);
+
+        return str;
     }
 }
--- CreatePhoneNumber/JavaVersion/src/com/company/Main.java
+++ CreatePhoneNumber/JavaVersion/src/com/company/Main.java
@@ -3,10 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
-Kata test = new Kata();
-test.createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
+        Kata test = new Kata();
+        test.createPhoneNumber(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0});
 //         Kata.createPhoneNumber(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}));
     }
 }
