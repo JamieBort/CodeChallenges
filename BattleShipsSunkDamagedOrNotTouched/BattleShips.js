3d95716c9b1c663725c42dbeff608e95c661c11c
Jamie Bort <jamiebort@gmail.com>
2018-09-22 14:04:44 -0400

    Working on Battle Ships. Determined the number of boats on teh board.

--- BattleShipsSunkDamagedOrNotTouched/BattleShips.js
+++ BattleShipsSunkDamagedOrNotTouched/BattleShips.js
@@ -1,12 +1,46 @@
-function damagedOrSunk (board, attacks){
-    //the battle starts here!
-    console.log(board);
-    console.log(attacks);
-  }
+function damagedOrSunk(board, attacks) {
+    // Counting the number of boats.
+    // Since each boat is represented by a number and the first boat starts with '1', then the highest number represents the number of boats.
+    // Therefore we need to parse the arrays in the array to find the highest digit.
 
-  var board = [ [0, 0, 1, 0],
-              [0, 0, 1, 0],
-              [0, 0, 1, 0] ];
-          
+    // Try this with foreach now.
+    // let myArray =[];
+    // let myNumber = 0;
+    // board.forEach(element => {
+    //     console.log(Math.max(Math.max(...element)));
+    //     myNumber = Math.max(Math.max(...element));
+    //     console.log(typeof myNumber);
+    //     myArray.push(myNumber);
+    // });
+    // console.log(myArray);
+    // console.log(Math.max(Math.max(...myArray)));
+
+    // Using a for loop.
+    var dummmyArray = [];
+    var NumberOfBoats = 0;
+    for (i=0; i<board.length; i++){
+        dummmyArray.push(Math.max(...board[i]));
+    }
+    NumberOfBoats = Math.max(...dummmyArray);
+
+    console.log(NumberOfBoats, "number of boats");
+    console.log(board, "board");
+    console.log(attacks, "attacks");
+}
+
+//   Game 1.
+var board = [[0, 0, 1, 0],
+[0, 0, 1, 0],
+[0, 0, 1, 0]];
+
 var attacks = [[3, 1], [3, 2], [3, 3]];
 var result = damagedOrSunk(board, attacks);
+
+// Game 2
+// var board = [ [3, 0, 1],
+//               [3, 0, 1],
+//               [0, 2, 1], 
+//               [0, 2, 0] ];
+
+// var attacks = [[2, 1], [2, 2], [ 3, 2], [3, 3]]
+// var result = damagedOrSunk(board, attacks)
