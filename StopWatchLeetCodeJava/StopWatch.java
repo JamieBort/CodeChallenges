c24aa3efffa5c616cf8774a277c08c96d84e62e6
Jamie Bort <jamiebort@gmail.com>
2018-10-14 12:01:38 -0400

    Added BlackJackLeetCodeJava and StopWatchLeetCodeJava directory and chalendges.

---
+++ StopWatchLeetCodeJava/StopWatch.java
@@ -0,0 +1,15 @@
+// Design Object Oriented class structure for a stop watch similar to the one that appears on a phone.
+// It should include features for the stopwatch, pause, cancel, reset and laps.
+
+// Create a class has function for 'pause', 'cancle', 'reset', and 'laps'.
+// We need to store two values for each lap, the time taken for that lap and the number of lap.
+// Create class for lap number, lap time, and finish time.
+// In our stopwatch class we can store a list of these objects.
+
+public class StopWatch(){
+    List Laps = new LinkedList(); // LinkedList uses a lot of pointers, called 'next'.
+    // List Laps = new ArrayList(); // List is an interface. Laps is the object name. ArrayList() is the concrete class name.
+    
+
+
+}
