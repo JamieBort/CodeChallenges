c51d24a874578c86ee8e8536964312152ebb30cc
Jamie Bort <jamiebort@gmail.com>
2019-02-01 10:45:23 -0500

    Added this file to work out a specific issue I was having with my file. Specifically to convert a time string into an integer.

---
+++ AmISafeToDriveCodeWarsJavaScript/TimeConvert.js
@@ -0,0 +1,10 @@
+// from: https://stackoverflow.com/questions/10893613/how-can-i-convert-time-to-decimal-number-in-javascript
+function timeStringToFloat(time) {
+    // var hoursMinutes =// convert to hours and minutes
+    // var hours = //
+    // var minutes = //
+    console.log("hello")
+    return ;//
+  }
+
+  timeStringToFloat("11:59");
