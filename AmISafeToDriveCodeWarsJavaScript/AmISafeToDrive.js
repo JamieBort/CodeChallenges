26ba03976310320a02c10bbf5a32de1078de2220
Jamie Bort <jamiebort@gmail.com>
2019-02-02 16:55:42 -0500

    Updated the file. Ran sample tests. A few failed.

--- AmISafeToDriveCodeWarsJavaScript/AmISafeToDrive.js
+++ AmISafeToDriveCodeWarsJavaScript/AmISafeToDrive.js
@@ -1,45 +1,57 @@
 function drive(drinks, finished, drive_time) {
+
     // On average it takes a person 1 hour for their body to remove 1 unit of alcohol.
-    
+
     // Data passed will be in the format ([[strength,volume]], finished, drive_time). Where finished is the time you stopped drinking and drive_time is the time you would like to drive.
-    const units = drinks[0][0] * drinks[0][1] / 1000; // Going through the drinks array to calculate the strength. Units of alcohol are calculated by strength (ABV) x volume (ml) / 1000 = units.
-
-    // If the finished >= drive_time then you can assume that it is the next day. <======== Revisit this.
-    
-
-    // const diff = 9 - 8;
-    // const diff = drive_time - finished;
-    const diff = [parseFloat(drive_time) - parseFloat(finished) ] * 1.00;
-    console.log("diff: ",diff);
-    var elapsed_time = 0;
-    elapsed_time = diff.toFixed(2); // Need to parse the two amounts into an integer. More solutions: https://stackoverflow.com/questions/6134039/format-number-to-always-show-2-decimal-places
-    console.log("diff with 2 dec. places: ",elapsed_time);
-    
-
-    console.log(typeof true);
-    console.log(typeof elapsed_time); // Elapsed time need to be a number, not a string.
-
-    var myNewNumber = Number(elapsed_time);
-    console.log("my new number: ",typeof myNewNumber, myNewNumber); // Need to convert to two decipmal places.
-
-    parseFloat(myNewNumber);
-    console.log("my new number: ",typeof myNewNumber, myNewNumber); // Need to convert to two decipmal places.
-
     // Return total units to 2 decimal places rounded. For example 1.10 => 1.1 and 1.236 => 1.24
-    // Return true if you are able to drive and false if you are not.
-    const myArray =[];
-    const myNumber = 1.25;
-    myArray.push(myNumber);
-    myArray.push(elapsed_time);
-    console.log("my array :", myArray);
+    const units = ((drinks[0][0] * drinks[0][1]) / 1000).toFixed(2); // Going through the drinks array to calculate the strength. Units of alcohol are calculated by strength (ABV) x volume (ml) / 1000 = units.
 
-    if (elapsed_time > units) {
-        console.log([elapsed_time, true]);
-        return [elapsed_time, true];
+    // console.log(units.toPrecision(3));
+
+    // converting the time string into total minutes as an integer.
+    const timeArray = [];
+    function timeStringToMinutesInteger(time) {
+        var firstVaribale = time.split(':');
+        const totalMinutes = parseInt(firstVaribale[0]) * 60 + parseInt(firstVaribale[1]);
+        timeArray.push(totalMinutes);
+    }
+    timeStringToMinutesInteger(finished);
+    timeStringToMinutesInteger(drive_time);
+    console.log("the time array: ", timeArray); // <------- Not needed.
+
+    // If the finished >= drive_time then you can assume that it is the next day.
+    if (timeArray[0] > timeArray[1]) {
+        console.log("it's the next day"); // <------- Not needed.
+        timeArray[0] = 24 * 60 - timeArray[0];
+        console.log(timeArray); // <------- Not needed.
+
+        // elapsedTime is the time difference in minutes between when the person stops drinking and when they want to start driving.
+        var elapsedTime = timeArray[1] + timeArray[0];
+    } else {
+        var elapsedTime = timeArray[1] - timeArray[0];
     }
 
-    console.log([elapsed_time, false]);
-    return [elapsed_time, false];
+    // Return true if you are able to drive and false if you are not.
+    console.log("the elapsed time is: ", elapsedTime); // <------- Not needed.
+    console.log("the units is: ", units); // <------- Not needed.
+    const expectedArray = [];
+    // expectedArray.push(elapsedTime);
+    expectedArray.push(units);
+    
+    // console.log(typeof(units)); // <------- Not needed.
+    if (elapsedTime <= 60 * units) {
+        console.log("Not enought time has passed");
+        expectedArray.push(false);
+    } else {
+        console.log("Enought time has passed");
+        expectedArray.push(true);
+    }
+    console.log(expectedArray); // <------- Not needed.
+    
+    
+    
+
+
 }
 
 // You will need to return the total units (to 2 decimal places) of the alcoholic drinks and a boolean value true if you are able to drive or false if you are still unable to drive.
