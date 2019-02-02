6df7c3572628e9bbbf1620fcd7bc83525de211ed
Jamie Bort <jamiebort@gmail.com>
2019-02-02 15:01:35 -0500

    Finished this component function.

--- AmISafeToDriveCodeWarsJavaScript/TimeConvert.js
+++ AmISafeToDriveCodeWarsJavaScript/TimeConvert.js
@@ -1,10 +1,31 @@
 // from: https://stackoverflow.com/questions/10893613/how-can-i-convert-time-to-decimal-number-in-javascript
-function timeStringToFloat(time) {
-    // var hoursMinutes =// convert to hours and minutes
-    // var hours = //
-    // var minutes = //
-    console.log("hello")
-    return ;//
-  }
+function timeStringToFloat(firstTime, secondTime) {
+  // split string into hours and minues. Assign each to a different variable.
 
-  timeStringToFloat("11:59");
+  // var hoursMinutes =// convert to hours and minutes
+  // var hours = //
+  // var minutes = //
+  console.log("hello");
+
+
+  var firstVaribale = firstTime.split(':');
+  var secondVaribale = secondTime.split(':');
+  console.log(firstVaribale);
+  console.log(secondVaribale);
+  var hourDifference = secondVaribale[0] - firstVaribale[0];
+  var secondDiffence = secondVaribale[1] - firstVaribale[1];
+  console.log(hourDifference);
+  console.log(secondDiffence);
+  if (secondDiffence < 0) {
+    console.log("something needs to be done!");
+    --hourDifference;
+    secondDiffence = -1 * secondDiffence;
+  }
+  console.log(hourDifference);
+  console.log(secondDiffence);
+  // console.log(words[0]);
+  // console.log(typeof(words[0]));
+  return;//
+}
+
+timeStringToFloat("11:59", "24:00");
