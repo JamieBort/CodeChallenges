37e6f191209a9f4f8d9df98f3d8d9bba1215eebd
Jamie Bort <jamiebort@gmail.com>
2019-02-05 01:18:47 -0500

    Updated IntegerToRoman/IntegerToRoman.js.

--- IntegerToRoman/IntegerToRoman.js
+++ IntegerToRoman/IntegerToRoman.js
@@ -62,58 +62,67 @@ var string = "";
 var intToRoman = function (num) {
     var multiples_of_M = (num - (num % M)) / M;
     var remainder_of_M = num % M;
-    console.log("Multiples of M: ",multiples_of_M);
-    console.log("The remainder is: ",remainder_of_M);
+    console.log("Multiples of M: ", multiples_of_M);
+    console.log("The remainder from M is: ", remainder_of_M);
     for (let i = 0; i < multiples_of_M; i++) {
         string += "M";
     }
 
     var multiples_of_D = (remainder_of_M - (remainder_of_M % D)) / D;
     var remainder_of_D = remainder_of_M % D;
-    console.log("Multiples of D: ",multiples_of_D);
-    console.log("The remainder is: ",remainder_of_D);
+    console.log("Multiples of D: ", multiples_of_D);
+    console.log("The remainder from D is: ", remainder_of_D);
     for (let i = 0; i < multiples_of_D; i++) {
         string += "D";
     }
 
     var multiples_of_C = (remainder_of_D - (remainder_of_D % C)) / C;
     var remainder_of_C = remainder_of_D % C;
-    console.log("Multiples of C: ",multiples_of_C);
-    console.log("The remainder is: ",remainder_of_C);
+    console.log("Multiples of C: ", multiples_of_C);
+    console.log("The remainder from C is: ", remainder_of_C);
     for (let i = 0; i < multiples_of_C; i++) {
         string += "C";
     }
 
     var multiples_of_L = (remainder_of_C - (remainder_of_C % L)) / L;
     var remainder_of_L = remainder_of_C % L;
-    console.log("Multiples of l: ",multiples_of_L);
-    console.log("The remainder is: ",remainder_of_L);
+    console.log("Multiples of L: ", multiples_of_L);
+    console.log("The remainder from L is: ", remainder_of_L);
     for (let i = 0; i < multiples_of_L; i++) {
         string += "L";
     }
 
     var multiples_of_X = (remainder_of_L - (remainder_of_L % X)) / X;
     var remainder_of_X = remainder_of_L % X;
-    console.log("Multiples of l: ",multiples_of_X);
-    console.log("The remainder is: ",remainder_of_X);
+    console.log("Multiples of X: ", multiples_of_X);
+    console.log("The remainder from X is: ", remainder_of_X);
     for (let i = 0; i < multiples_of_X; i++) {
         string += "X";
     }
 
     var multiples_of_V = (remainder_of_X - (remainder_of_X % V)) / V;
     var remainder_of_V = remainder_of_X % V;
-    console.log("Multiples of l: ",multiples_of_V);
-    console.log("The remainder is: ",remainder_of_V);
-    for (let i = 0; i < multiples_of_V; i++) {
-        string += "V";
+    console.log("Multiples of V: ", multiples_of_V);
+    console.log("The remainder from V is: ", remainder_of_V);
+    if (multiples_of_V === 9) {
+        // string += "IV";
+        console.log("this is bad!!!!!")
+    } else {
+        for (let i = 0; i < multiples_of_V; i++) {
+            string += "V";
+        }
     }
 
     var multiples_of_I = (remainder_of_V - (remainder_of_V % I)) / I;
     var remainder_of_I = remainder_of_V % I;
-    console.log("Multiples of l: ",multiples_of_I);
-    console.log("The remainder is: ",remainder_of_I);
-    for (let i = 0; i < multiples_of_I; i++) {
-        string += "I";
+    console.log("Multiples of I: ", multiples_of_I);
+    console.log("The remainder is: ", remainder_of_I);
+    if (multiples_of_I === 4) {
+        string += "IV";
+    } else {
+        for (let i = 0; i < multiples_of_I; i++) {
+            string += "I";
+        }
     }
 
 
@@ -127,7 +136,7 @@ var intToRoman = function (num) {
     console.log(string);
 };
 
-intToRoman(1888);
+intToRoman(9);
 
 // var string = "";
 // console.log("this is my string: ",string);
