59bfea3d4842cdb8a0134a4ac84284d67db0546b
Jamie Bort <jamiebort@gmail.com>
2019-02-05 10:27:34 -0500

    Started over to do this on my own. Mostly done.

--- FindAllArrayValuesThatFallWithinAGivenDifferenceCodeWarsJavaScript/GivenDifference.js
+++ FindAllArrayValuesThatFallWithinAGivenDifferenceCodeWarsJavaScript/GivenDifference.js
@@ -1,51 +1,43 @@
-class GroupByDifference {
-    constructor(numbers) {
-        this.numbers = numbers; // Why this line of code? Why not ommit it? Or 'numbers = numbers'?
-    };
-    find(diff) {
+function withIn(array, num) {
 
-        // console.log(this.numbers);
-        this.difference = diff; // Seems like 'this.difference = diff;' should be 'this.diff = difference;' instead. If I am reading it correctly it's assigning 'diff' to 'this.difference'. 
 
-        // Initialize an output array. 
-        let output = [];
-        let indices = [];
-        // Iterate through 'this.numbers'.
-        for (let i = 0; i < this.numbers.length; i++) {
-            // In a nested loop, iterate again through this.numbers.
-            for (let j = 0; j < this.numbers.length; j++) {
-                // If the absolute value of numbers[j] - numbers[i] <= difference, push numbers[j] into the output array.
-                // console.log(this.numbers[j] + ' - ' + this.numbers[i] + " = " + Math.abs(this.numbers[j] - this.numbers[i]));
-                if (Math.abs(this.numbers[j] - this.numbers[i]) <= diff && (i != j)) {
-                    // console.log("pushing " + this.numbers[j]);
-                    indices.push(i);
-                }
+    // want to itterate through and count down
+    for (let i = array.length - 1; i > 0; i--) {
+        console.log("i", i);
+        for (let j = array.length - 1; j > 0; j--) {
+            console.log("j", j);
+            var minArray = [];
+            if (Math.abs(array[array.length - 1] - array[i]) < num) {
+                console.log("The numbers are close. The difference is ", Math.abs(array[array.length - 1] - array[i]));
+                // minArray.push(array[i]);
+                // array.pop(i);
+            } else {
+                console.log("the numbers are NOT close! The difference is ", Math.abs(array[array.length - 1] - array[i]));
             }
         }
-
-        for (let t = 0; t < indices.length; t++) {
-            for (let s = t + 1; s < indices.length; s++) {
-                if (indices[t] == indices[s]) {
-                    indices.splice(s, 1);
-                    s--;
-                }
-            }
-        }
-
-        for (let r = 0; r < indices.length; r++) {
-            output.push(this.numbers[indices[r]]);
-        }
-
-        // sorting the output array
-        output.sort(function compareFunction(a, b) { // Can be annonomous. I included 'compareFunction' to help reinforce the syntax.
-            return a - b;
-          });
-        
-        // Return output array.
-        return output;
     }
+    // const minArray = [];
+    // if (Math.abs(array[8] - array[7]) < num) {
+    //     console.log("the numbers are close. The difference is ", Math.abs(array[0] - array[1]));
+    //     minArray.push(array[0]);
+    //     minArray.push(array[1]);
+    // } else {
+    //     console.log("the numbers are NOT close! The difference is ", Math.abs(array[0] - array[1]));
+    // }
+
+
+
+    console.log(minArray);
+    return minArray;
 }
-let nums = [5, 32, 5, 1, 31, 70, 30, 8];
-let difference = 2;
-let foo = new GroupByDifference(nums);
-console.log(foo.find(difference));
+
+withIn([1, 31, 30], 2);
+
+// withIn([5, 32, 5, 1, 31, 70, 30, 8], 2);
+
+// We need to find the difference between all the number that are less than 3.
+
+
+// Given [5, 32, 5, 1, 31, 70, 30, 8], 
+// and you want to know all the values that fall within a difference of 2 of each other:
+// Should return [5,5,30,31,32]
